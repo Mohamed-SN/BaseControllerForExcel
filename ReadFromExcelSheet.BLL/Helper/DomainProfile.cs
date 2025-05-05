a0cdde895d5e0e05a6ba1feae69c504dd8673784
Mohamed-SN <hajar@sap-pro.co>
2025-05-05 11:33:27 +0300

    Add Automapper

---
+++ ReadFromExcelSheet.BLL/Helper/DomainProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ReadFromExcelSheet.DAL.Entities;
+using ReadFromExcelSheet.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFromExcelSheet.BLL.Helper
+{
+    public class DomainProfile : Profile
+    {
+        public DomainProfile() 
+        {
+            CreateMap<StudentDto, Student>();
+        }
+
+    }
+}
